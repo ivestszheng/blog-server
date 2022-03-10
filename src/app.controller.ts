c0753bad2e198cf626d4e06a41d8441fb5a702a9
无声 <ivestszheng@qq.com>
2022-03-10 14:23:41 +0800

    feat: 初步接口测试

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('user_*')
+  getUser() {
+    return 'getUser';
+  }
+
+  @Put('list/user')
+  updateUser() {
+    return { userId: 1 };
+  }
+
+  // 3.带参数路径
+  // 可以匹配到put请求，http://localhost:9080/app/list/xxxx
+  @Put('list/:id')
+  update() {
+    return 'update';
   }
 }
