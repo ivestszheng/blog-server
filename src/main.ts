41559e00f4e7dd29fb3ca881de6eb6344a0f9bea
无声 <ivestszheng@qq.com>
2022-03-10 17:11:26 +0800

    fix: 数据验证功能报错

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { HttpExceptionFilter } from './core/filter/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './core/interceptor/transform.interceptor';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
