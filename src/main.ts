5aa77d919f181dbaa0b386ae06e5154f6f8d8429
无声 <ivestszheng@qq.com>
2022-03-10 16:36:31 +0800

    feat: 增加过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './core/filter/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // 设置全局路由前缀
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
